9aeaa373268fb32f84a00b1eb42ac97ffd7ab7c5
Peter <1914571088@qq.com>
2018-10-13 02:05:17 +0800

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,6 +8,10 @@ module.exports = {
     "pid_file"        : "/var/pid/app.pid",
     "merge_logs"      : true,
     "log_date_format" : "YYYY-MM-DD HH:mm Z",
+    "watch": true,
+    "watch_options": {
+        "usePolling": true
+    },
     env: {
       NODE_ENV: 'development'
     },
@@ -15,15 +19,5 @@ module.exports = {
       NODE_ENV: 'production'
     },
      // max_memory_restart:500
-  }],
-  deploy : {
-    production : {
-      user : 'root',
-      host : '207.148.66.211',
-      ref  : 'origin/master',
-      repo : 'git@github.com:cheaprYang/webpack4_ssr.git',
-      path : '/var/www/webpack_demo',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-    }
-  }
+  }]
 };
