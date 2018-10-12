9345f30c62069754a44669e98aa9e19903f9546e
Peter <1914571088@qq.com>
2018-10-12 16:42:00 +0800

    fix

--- server/index.js
+++ server/index.js
@@ -67,7 +67,7 @@ const serve = (path, cache) => express.static(resolve(path), {
 app.use(compression({ threshold: 0 }))
 //app.use(favicon('./public/logo-48.png'))
 app.use('/dist', serve('../dist', true))
- // app.use('/static', serve('../static', true))
+  app.use('/static', serve('../dist/static', true))
 app.use('/manifest.json', serve('../manifest.json', true))
 app.use('/service-worker.js', serve('../dist/service-worker.js'))
 
